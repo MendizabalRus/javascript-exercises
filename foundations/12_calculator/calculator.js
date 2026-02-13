dd7a38ea919a46914049e0bf7ffde32d48435d76
MendizabalRus <243220275+MendizabalRus@users.noreply.github.com>
2026-02-13 17:42:50 +0100

    ex1-2Calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -2,24 +2,40 @@ const add = function(a, b) {
 	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((acc, number) => {
+    return acc + number;
+    }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((acc, number) => {
+    return acc * number;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a == 0) {
+    return 1;
+  } 
+  else {
+    let iterations = [];
+    for (let i = 1; i <= a; i++) {
+      let results = i;
+      iterations.push(results);
+    }
+    return iterations.reduce((acc, number) => {
+      return acc * number;
+    }, 1);
+  }
 };
 
 // Do not edit below this line
