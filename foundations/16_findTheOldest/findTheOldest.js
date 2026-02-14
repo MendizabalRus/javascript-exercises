34845eb3a127d40b1c4a4c4c6f69ce08fabcc575
MendizabalRus <243220275+MendizabalRus@users.noreply.github.com>
2026-02-14 18:09:11 +0100

    doneFindTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,34 @@
-const findTheOldest = function() {
+const people = [
+    {
+        name: "Carly",
+        yearOfBirth: 1942,
+        yearOfDeath: 1970,
+    },
+    {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+    },
+    {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+    }
+];
 
+const findTheOldest = function(people) {
+    return people.reduce((oldestPerson, nextPerson) => {
+
+        const oldestDeathYear = oldestPerson.yearOfDeath || new Date().getFullYear();
+        const oldestBirthYear = oldestPerson.yearOfBirth
+        const oldestAge = oldestDeathYear - oldestBirthYear;
+
+        const nextDeathYear = nextPerson.yearOfDeath || new Date().getFullYear();
+        const nextBirthYear = nextPerson.yearOfBirth
+        const nextAge = nextDeathYear - nextBirthYear;
+
+        return (oldestAge > nextAge ? oldestPerson : nextPerson);
+    });
 };
 
 // Do not edit below this line
